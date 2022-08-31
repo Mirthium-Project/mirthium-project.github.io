3c60632d0dda2dc9d3229d16b065c161a8b64cc3
kpalang <kpalang@mirthium.org>
2022-08-31 11:27:38 +0300

    Remove comments and added a ToDo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,14 +14,9 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Mirthium-Project', // Usually your GitHub org/user name.
-  projectName: 'mirthium-project.github.io', // Usually your repo name.
+  organizationName: 'Mirthium-Project',
+  projectName: 'mirthium-project.github.io',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -34,10 +29,8 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // TODO: Fix this link to properly point to an Edit Page guide
+          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: false,
         theme: {
